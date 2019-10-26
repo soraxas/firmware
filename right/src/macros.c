c380246c9e80bc3fe5485bfb2057d8190181bdf2
Karel Tuček <kareltucek@centrum.cz>
2019-10-26 09:30:44 +0200

    - fix an old weird glitch

--- right/src/macros.c
+++ right/src/macros.c
@@ -2372,6 +2372,7 @@ void Macros_StartMacro(uint8_t index, key_state_t *keyState, uint8_t parentMacro
 bool continueMacro(void)
 {
     PostponeKeys |= s->postponeNext > 0;
+    s->postponingNow = false;
     if (processCurrentMacroAction() && !s->macroBroken) {
         //if action consists of multiple subactions, break here
         return true;
